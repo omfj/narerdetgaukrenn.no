e3af579ce51b6c9c63ace702121db18b797ad016
Ole Magnus Fon Johnsen <me@omfj.no>
2024-03-30 00:47:47 +0100

    Add logs

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -4,6 +4,11 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async () => {
 	const [teams, challenges] = await Promise.all([api.teams.get.all(), api.challenges.get.all()]);
 
+	console.log(`Loading teams: ${teams.map((team) => team.name).join(', ') || 'none'}.`);
+	console.log(
+		`Loading challenges: ${challenges.map((challenge) => challenge.name).join(', ') || 'none'}.`
+	);
+
 	return {
 		teams,
 		challenges
--- src/routes/leaderboard/+page.server.ts
+++ src/routes/leaderboard/+page.server.ts
@@ -4,6 +4,8 @@ import { api } from '$lib/api';
 export const load: PageServerLoad = async () => {
 	const teams = await api.teams.get.all();
 
+	console.log(`Loading teams: ${teams.map((team) => team.name).join(', ') || 'none'}.`);
+
 	return {
 		teams
 	};
