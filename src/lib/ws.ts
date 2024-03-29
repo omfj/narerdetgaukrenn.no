41415443086bc67ff4b679bca0a602902a3200ce
Ole Magnus Fon Johnsen <me@omfj.no>
2024-03-30 00:37:04 +0100

    Use secure ws

--- src/lib/ws.ts
+++ src/lib/ws.ts
@@ -1,3 +1,6 @@
+import { dev } from '$app/environment';
 import { PUBLIC_BACKEND_URL } from '$env/static/public';
 
-export const BACKEND_WS_URL = `ws://${new URL(PUBLIC_BACKEND_URL).host}/ws/teams`;
+const WS = dev ? 'ws' : 'wss';
+
+export const BACKEND_WS_URL = `${WS}://${new URL(PUBLIC_BACKEND_URL).host}/ws/teams`;
